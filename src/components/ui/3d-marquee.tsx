8eeae08444d2575ee902712bbc1ed910e309038a
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2025-07-05 15:12:16 +0100

    auth pages

--- src/components/ui/3d-marquee.tsx
+++ src/components/ui/3d-marquee.tsx
@@ -18,12 +18,12 @@ export const ThreeDMarquee = ({
   return (
     <div
       className={cn(
-        "mx-auto block h-[600px] overflow-hidden rounded-2xl max-sm:h-100",
+        "mx-auto block min-h-[600px] h-full overflow-hidden rounded-2xl max-sm:h-100",
         className,
       )}
     >
       <div className="flex size-full items-center justify-center">
-        <div className="size-[1720px] shrink-0 scale-50 sm:scale-75 lg:scale-100">
+        <div className="size-[2300px] shrink-0 scale-50 sm:scale-75 lg:scale-100">
           <div
             style={{
               transform: "rotateX(55deg) rotateY(0deg) rotateZ(-45deg)",
